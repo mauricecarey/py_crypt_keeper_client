2112f877d689aa463ff735d5904697d3d30f8f47
Maurice Carey <maurice@mauricecarey.com>
2016-10-15 02:11:30 -0700

    Fixing file formatting.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
-setup(name='py_crypt_keeper_client',
-      version='0.1',
-      description='A Python client to the Crypt-Keeper service.',
-      url='http://github.com/mauricecarey/py_crypt_keeper_client',
-      author='Maurice Carey',
-      author_email='maurice@mauricecarey.com',
-      license='Apache 2.0',
-      packages=['py_crypt_keeper_client'],
-      install_requires=[
-          'pycrypto==2.6.1'
-      ],
-      zip_safe=False)
+setup(
+    name='py_crypt_keeper_client',
+    version='0.1',
+    description='A Python client to the Crypt-Keeper service.',
+    url='http://github.com/mauricecarey/py_crypt_keeper_client',
+    author='Maurice Carey',
+    author_email='maurice@mauricecarey.com',
+    license='Apache 2.0',
+    packages=['py_crypt_keeper_client'],
+    install_requires=[
+        'pycrypto==2.6.1',
+    ],
+    zip_safe=False
+)
