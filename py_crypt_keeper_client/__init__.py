b4e5d4891b74ccce7cd523cd6275245d2b9dbd17
Maurice Carey <maurice@mauricecarey.com>
2016-10-14 23:06:31 -0700

    Adding method to get the download url and skeleton for download file.

--- py_crypt_keeper_client/__init__.py
+++ py_crypt_keeper_client/__init__.py
@@ -73,6 +73,27 @@ class CryptKeeperClient(object):
             log.exception('HTTP Request failed!', e)
         return None
 
+    def get_download_url(self, document_id):
+        try:
+            url = '%s/download_url/%s/' % (self.url, document_id)
+            log.debug('Trying URL: %s', url)
+            response = requests.get(
+                url=url,
+                headers={
+                    'Accept': 'application/json',
+                    'Authorization': 'ApiKey %s:%s' % (self.user, self.api_key),
+                },
+            )
+            log.debug('Response HTTP Status Code: {status_code}'.format(
+                status_code=response.status_code))
+            log.debug('Response HTTP Response Body: {content}'.format(
+                content=response.content))
+            if response.status_code == 200:
+                return json.loads(response.content.decode('utf-8'))
+        except requests.exceptions.RequestException as e:
+            log.exception('HTTP Request failed!', e)
+        return None
+
     def upload_file(self, filename):
         file_size = getsize(filename)
         document_metadata = {
@@ -98,3 +119,8 @@ class CryptKeeperClient(object):
         except requests.exceptions.RequestException as e:
             log.exception('HTTP Request failed!', e)
         return None
+
+    def download_file(self, document_id):
+        # http://docs.python-requests.org/en/master/api/#requests.Response.iter_content
+        # use requests.get with stream=True to get chunk-encoded responses.
+        pass
